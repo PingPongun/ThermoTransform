d83b42bafcf036ea73c80d1083b79cfc3737ce90
Dawid Lentka <46752179+PingPongun@users.noreply.github.com>
2023-02-11 18:25:53 +0100

    fixes( time out of range; wrong time/scale ranges after switching time/transform view)

--- src/thermo_backend.rs
+++ src/thermo_backend.rs
@@ -64,7 +64,15 @@ impl Default for RangedVal
         }
     }
 }
-#[derive(Clone, Copy, PartialEq, Debug, strum_macros::AsRefStr, EnumString, EnumVariantNames)]
+#[derive(
+    Clone,
+    Copy,
+    PartialEq,
+    Debug,
+    strum_macros::IntoStaticStr,
+    strum_macros::AsRefStr,
+    EnumVariantNames,
+)]
 #[strum(serialize_all = "title_case")]
 pub enum TTViewParams
 {
@@ -80,6 +88,20 @@ pub enum TTViewParams
         time : RangedVal
     },
 }
+trait EnumUpdate<T>
+{
+    fn update(&mut self, new_val : &str);
+}
+
+impl<T> EnumUpdate<T> for T
+where
+    T : VariantNames,
+    T : AsRef<str>,
+    T : FromStr,
+    <T as FromStr>::Err : std::fmt::Debug,
+{
+    fn update(&mut self, new_val : &str) { *self = T::from_str(new_val).unwrap(); }
+}
 trait EnumCombobox<T>
 {
     fn show_combobox(&mut self, ui : &mut egui::Ui) -> bool;
@@ -88,8 +110,8 @@ impl<T> EnumCombobox<T> for T
 where
     T : VariantNames,
     T : AsRef<str>,
-    T : FromStr,
-    <T as FromStr>::Err : std::fmt::Debug,
+    T : EnumUpdate<T>,
+    T : Clone,
 {
     fn show_combobox(&mut self, ui : &mut egui::Ui) -> bool
     {
@@ -102,9 +124,10 @@ where
         ui.set_min_width(10.0);
         egui::ComboBox::from_id_source(ui.next_auto_id())
             .selected_text(self.as_ref())
-            .icon(|ui, _, _, _, _| {})
+            .icon(|_, _, _, _, _| {})
             .show_ui(ui, |ui| {
-                let mut current = self.as_ref();
+                let cloneself = self.clone();
+                let mut current = cloneself.as_ref();
                 ui.style_mut().wrap = Some(false);
                 for &var in T::VARIANTS
                 {
@@ -115,12 +138,72 @@ where
                 }
                 if retval
                 {
-                    *self = T::from_str(current).unwrap();
+                    self.update(current);
                 }
             });
         retval
     }
 }
+impl EnumUpdate<TTViewParams> for TTViewParams
+{
+    fn update(&mut self, new_val : &str)
+    {
+        let timeview : &'static str = (TimeView {
+            time : Default::default(),
+        })
+        .into();
+        let transformview : &'static str = (TransformView {
+            time :    Default::default(),
+            scale :   Default::default(),
+            wavelet : Default::default(),
+            mode :    Default::default(),
+        })
+        .into();
+        let frames;
+        match self
+        {
+            TimeView { time } => frames = time.max + 1,
+            TransformView {
+                scale,
+                time: _,
+                wavelet: _,
+                mode: _,
+            } => frames = scale.max,
+        };
+        if timeview == new_val
+        {
+            *self = TimeView {
+                time : RangedVal {
+                    val : 0,
+                    min : 0,
+                    max : frames - 1,
+                },
+            }
+        }
+        else if transformview == new_val
+        {
+            *self = TransformView {
+                time :    RangedVal {
+                    val : 0,
+                    min : 0,
+                    max : frames - 1,
+                },
+                scale :   RangedVal {
+                    val : 1,
+                    min : 1,
+                    max : frames,
+                },
+                wavelet : Default::default(),
+                mode :    Default::default(),
+            }
+        }
+        else
+        {
+            unreachable!()
+        }
+    }
+}
+
 impl TTViewParams
 {
     ///#Returns (a,b)
@@ -446,7 +529,7 @@ impl TTStateGUI
                                 }
                                 TimeView { mut time } =>
                                 {
-                                    time.max = frames;
+                                    time.max = frames - 1;
                                     params = TimeView { time };
                                 }
                             }
