8f444fa3ee982afa872badda405d58fa36569f23
Dawid Lentka <46752179+PingPongun@users.noreply.github.com>
2023-03-06 18:29:00 +0100

    implement cwt

--- src/cwt.rs
+++ src/cwt.rs
@@ -134,6 +134,105 @@ impl TTLazyCWT
     pub fn cwt(&self, wavelet_bank : &mut WaveletBank, params : &TransformViewParams)
         -> Array2<f64>
     {
+        let wavelet = wavelet_bank.get_mut(&params.wavelet).unwrap(); //`WaveletBank` should have entries for all WaveletType-enum wariants, if not this is an error in code so panic is apropriate
+        let polywise = wavelet.get_poly_wise(params.scale.val);
+        let shape = self.integrals.dim();
+        let t = params.time.val as isize;
+        let v = self
+            .integrals
+            .lanes(Axis(0))
+            .into_iter()
+            .into_par_iter()
+            .map(|integrals| {
+                let real_img : Vec<f64> = polywise
+                    .0
+                    .iter()
+                    .map(|polywise| {
+                        let mut accum : f64 = 0.0;
+                        let s0 = t + polywise.dxpsi[0].x;
+                        let sJ = t + polywise.dxpsi[1].x;
+                        let integral_s0;
+                        let integral_sJ;
+                        let last_idx = integrals.len() - 1;
+                        let last_idx_isize = (integrals.len() - 1) as isize;
+                        if s0 < 0
+                        {
+                            //wavelet "goes beyond" signal => extend signal using antisymetric whole-point extension
+                            //x0, x1, x2 -> (2*x0-x2), (2*x0-x1), x0, x1, x2
+                            let s0 = (-s0) as usize;
+                            integral_s0 = [
+                                f64::mul_add(integrals[0][0], 2.0, integrals[s0][0]),
+                                f64::mul_add(integrals[0][1], 2.0, integrals[s0][1]),
+                                f64::mul_add(integrals[0][2], 2.0, integrals[s0][2]),
+                            ];
+                        }
+                        else
+                        {
+                            let s0 = s0 as usize;
+                            integral_s0 = [integrals[s0][0], integrals[s0][1], integrals[s0][2]];
+                        }
+                        if sJ > last_idx_isize
+                        {
+                            //wavelet "goes beyond" signal ...
+                            let sJ = (2 * last_idx_isize - sJ) as usize;
+                            integral_sJ = [
+                                f64::mul_add(integrals[last_idx][0], 2.0, integrals[sJ][0]),
+                                f64::mul_add(integrals[last_idx][1], 2.0, integrals[sJ][1]),
+                                f64::mul_add(integrals[last_idx][2], 2.0, integrals[sJ][2]),
+                            ];
+                        }
+                        else
+                        {
+                            let sJ = sJ as usize;
+                            integral_sJ = [integrals[sJ][0], integrals[sJ][1], integrals[sJ][2]];
+                        }
+                        accum = f64::mul_add(integral_s0[0], polywise.dxpsi[0].y[0], accum);
+                        accum = f64::mul_add(integral_s0[1], polywise.dxpsi[0].y[1], accum);
+                        accum = f64::mul_add(integral_s0[2], polywise.dxpsi[0].y[2], accum);
+                        accum = f64::mul_add(integral_sJ[0], polywise.dxpsi[1].y[0], accum);
+                        accum = f64::mul_add(integral_sJ[1], polywise.dxpsi[1].y[1], accum);
+                        accum = f64::mul_add(integral_sJ[2], polywise.dxpsi[1].y[2], accum);
+                        polywise.d3psi.iter().for_each(|point| {
+                            let s = t + point.x;
+                            if s < 0
+                            {
+                                accum = f64::mul_add(
+                                    f64::mul_add(integrals[0][3], 2.0, integrals[(-s) as usize][3]),
+                                    point.y,
+                                    accum,
+                                )
+                            }
+                            else if s > last_idx_isize
+                            {
+                                accum = f64::mul_add(
+                                    f64::mul_add(
+                                        integrals[last_idx][3],
+                                        2.0,
+                                        integrals[(2 * last_idx_isize - s) as usize][3],
+                                    ),
+                                    point.y,
+                                    accum,
+                                )
+                            }
+                            else
+                            {
+                                accum = f64::mul_add(integrals[s as usize][3], point.y, accum)
+                            }
+                        });
+                        accum
+                    })
+                    .collect();
+                //convert to requested format
+                match params.mode
+                {
+                    WtResultMode::Phase => real_img[1].atan2(real_img[0]), //radians
+                    WtResultMode::Magnitude => real_img[0].hypot(real_img[1]),
+                    WtResultMode::Real => real_img[0],
+                    WtResultMode::Imaginary => real_img[1],
+                }
+            })
+            .collect();
+        Array2::from_shape_vec((shape.1, shape.2), v).unwrap();
         Array2::default((self.integrals.dim().1, self.integrals.dim().2))
     }
 }
